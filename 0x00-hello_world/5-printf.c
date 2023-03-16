bafff1bae833740eee7ddb7c7a648a98d0a9296a
hassan <hassan.winners@gmail.com>
2023-03-16 16:04:31 -0700

    Fixing he task #6 and #5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main -  Create main
+ *
+ * Description: a C program that print Str with printf func
+ *
+ * Return: vlaue 0
+*/
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
